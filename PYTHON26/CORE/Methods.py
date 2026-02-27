f9b277bc9e18d05deb85de698f81da75c4bb533a
HEMA RAO <pamdarihemaraopandari@gmail.com>
2026-02-27 10:08:54 +0530

    Init commit

--- PYTHON26/CORE/Methods.py
+++ PYTHON26/CORE/Methods.py
@@ -1,14 +1,43 @@
-class profile:
-    def __init__(self,username):
-        self.followers=0
-        self.username=("_hema_p")
-    def follow(self):
-        print("someone followed you")
-        self.followers+=1
-    def update_username(self,new_user_name):
-        self.username=new_user_name
-p1=profile("_hema_p")
-p1.follow()
-print(p1.followers)
-p1.update_username("p_hema")
-print(p1.username)
+# class profile:
+# #     def __init__(self,username):
+# #         self.followers=0
+# #         self.username=("_hema_p")
+# #     def follow(self):
+# #         print("someone followed you")
+# #         self.followers+=1
+# #     def update_username(self,new_user_name):
+# #         self.username=new_user_name
+# # p1=profile("_hema_p")
+# # p1.follow()
+# # print(p1.followers)
+# # p1.update_username("p_hema")
+# print(p1.username)
+from winreg import HKEY_LOCAL_MACHINE
+
+
+# class Building:
+#     no_of_rooms=7
+#     Total_Buildings=0
+#     def __init__(self,Building_name,Wifi_name):
+#         self.Building_name="hema_rao"
+#         self.Wifi_name=Wifi_name
+#         Building.Total_Buildings+=1
+# b1=Building("Engineering",True)
+# b2=Building("jhtuh",False)
+# print("Total Buildings:",Building.Total_Buildings)
+# print("Default_Rooms:",Building.no_of_rooms)
+# print("Building 1:",b1.Building_name,b1.Wifi_name)
+
+# class Person:
+#     no_of_phones=0
+#     def __init__(self,Name,age,phone_no):
+#         self.Name=Name
+#         self.age=age
+#         self.phone_no=phone_no
+#         if phone_no==True:
+#             Person.no_of_phones+=1
+# P1=Person("hema",20, True)
+# P2=Person("Lahari",21,False)
+# print(P1.Name,P1.age,P1.phone_no)
+# print(P2.Name,P2.age,P2.phone_no)
+
--- PYTHON26/CORE/TEST.py
+++ PYTHON26/CORE/TEST.py
@@ -1,1 +1,62 @@
-print("hello world")
+#l=[[1,2],[3,4],[5,6]],
+#k=list(map(lambda x: x+[5], l))
+#print(k)
+#d={"apple":100,"banana":40,"cherry":150}
+#print(d.keys())
+#print(d.values())
+#print(d.items())
+#f=dict(filter(lambda x:x[1]>50,d.items()))
+#print(f)
+#k=list(map(int,input().split()))
+#m= 10**6
+#for i in k:
+  #  if m<i:
+ #      m=i
+
+#from functools.reduce(lambda a,b: a if a>b else b, k)
+#print(k)
+from functools import reduce
+#l=[5,10,15,20,25,30]
+#sq=list(map(lambda x:x*
+
+#di=list(filter(lambda x:x%5==0,sq))
+##v=reduce(lambda x,y:x+y,di)
+#print(v)
+#4th question
+ #k = reduce(lambda x,y: x+y, [1,2,3])
+#5th question
+#def _warn_python_reduce_kwargs(func):
+#6th question
+# v ="AEIOUaeiou"
+# st = list(filter(lanbda x: x not in v, input()))
+# print(st)
+#7th answer
+#l =['p','y','t','h','o','n']
+#st = list(filter(lambda x,y: x+y, l)
+#print(st)
+#8th question
+# l = [10,350,10,20,350]
+#k = list(map(lambda x: id(x), l))
+#print(k)
+#2nd module,1st question
+#k = list(map(lambda x,y,z: x+y+z, [1,2,3],[4,5,6],[]))
+#print(k)
+#2nd question
+#a = [1,2,3,4]
+#b = [10,20,30,40]
+#k = list(map(lambda x,y:x+y,a,b))
+#print(k)
+#3rd question
+#nums = [12,15,7,18,20,21,25]
+#l = list(filter(lambda x: (x%3==0) ^ (x%5==0), nums))
+#print(l)
+#5th Question
+#nums = [[1,2], [3,4], [5,6]]
+#result = list(map(lambda x:x, append(10), nums))
+#print(result, nums, sep='\n')
+
+
+
+
+
+
